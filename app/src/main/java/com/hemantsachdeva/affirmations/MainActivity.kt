783b144e7c7949b83bfca6ab2f0dab7b0ac4c589
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-12-05 17:23:25 +0530

    Init empty activity
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

---
+++ app/src/main/java/com/hemantsachdeva/affirmations/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hemantsachdeva.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
